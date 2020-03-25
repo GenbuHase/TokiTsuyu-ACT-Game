ef9f09f0d48f0b8e5a7f0c0c719607b966059c0b
Genbu Hase <14960841+GenbuHase@users.noreply.github.com>
2020-03-25 11:40:18 +0900

    0.1.2 + MITライセンスを適応 + p5をインスタンス方式に変更   - public/sketchesを削除 + 数種のアセットを追加 * 仮コードの除去

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,59 @@
 <template>
-	<Div />
+	<Div ID = "App" />
 </template>
 
 <style lang = "scss">
+	body {
+		overflow: hidden;
+	}
+
+	#App canvas {
+		display: block;
+		margin: 0 auto;
+
+		border: 1px solid gray;
+	}
 </style>
+
+<script>
+	import { p5Plus } from "@/libs/p5.plus";
+
+	const TEXTURES = {
+		Player: {
+			Standing: null,
+			Walking: null,
+			Running: null
+		}
+	};
+
+
+	let player;
+
+	const sketch = p => {
+		p.preload = () => {
+			p5Plus(p);
+
+			TEXTURES.Player.Standing = p.loadAnimation("/assets/characters/girl_running--001.png", "/assets/characters/girl_running--003.png");
+			TEXTURES.Player.Standing.frameDelay = 10;
+		};
+
+		p.setup = () => {
+			for (const img of TEXTURES.Player.Standing.images) img.width = p.windowMinSize/4, img.height = p.windowMinSize/3;
+
+			p.createCanvas(p.windowMinSize, p.windowMinSize);
+
+			player = p.createSprite(p.mouseX, p.mouseY);
+			player.addAnimation("player", TEXTURES.Player.Standing);
+		};
+
+		p.draw = () => {
+			p.background(255, 255, 255);
+
+			player.position = p.createVector(p.mouseX, p.mouseY);
+
+			p.drawSprites();
+		};
+	};
+
+	new p5(sketch, "App");
+</script>
--- src/pages/tools/StageEditor/index.vue
+++ src/pages/tools/StageEditor/index.vue
@@ -1,11 +1,58 @@
 <template>
-	<Div>
-		<Script Src = "/sketches/StageEditor.js" />
-	</Div>
+	<Div ID = "App" />
 </template>
 
 <style lang = "scss">
 	body {
 		overflow: hidden;
 	}
+
+	#App canvas {
+		display: block;
+		margin: 0 auto;
+
+		border: 1px solid gray;
+	}
 </style>
+
+<script>
+	import { p5Plus } from "@/libs/p5.plus";
+
+	const TEXTURES = {
+		Player: {
+			Standing: null,
+			Walking: null,
+			Running: null
+		}
+	};
+
+
+	let player;
+
+	const sketch = p => {
+		p.preload = () => {
+			p5Plus(p);
+
+			TEXTURES.Player.Standing = p.loadImage("/assets/characters/girl-standing.png", img => {
+				img.width = 96, img.height = 128;
+			});
+		};
+
+		p.setup = () => {
+			p.createCanvas(p.windowMinSize, p.windowMinSize);
+
+			player = p.createSprite(p.mouseX, p.mouseY, 48, 64);
+			player.addImage(TEXTURES.Player.Standing);
+		};
+
+		p.draw = () => {
+			p.background(255, 255, 255);
+
+			player.position = p.createVector(p.mouseX, p.mouseY);
+
+			p.drawSprites();
+		};
+	};
+
+	new p5(sketch, "App");
+</script>
